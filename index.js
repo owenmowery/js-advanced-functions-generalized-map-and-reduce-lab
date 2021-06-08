61aae2fc432b973612e3aca504b0b360533d280d
Owen Mowery <owenmowery99@gmail.com>
2021-06-08 12:46:11 -0400

    commit tree c63f7ae310adfa7fc829d74a52d549e04e93d17f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your functions here
+import { start } from "repl";
+
+function map(array, callback) {
+    let newArr = [];
+    for (let i = 0; i < array.length; i++) {
+        newArr.push(callback(array[i]));
+    }
+    return newArr;
+}
+
+function reduce(arr, callback, startPoint) {
+    let runningTotal;
+    let i;
+    if (startPoint) {
+        runningTotal = startPoint;
+        i = 0;
+    } else {
+        runningTotal = arr[0];
+        i = 1;
+    }
+
+    for (i; i < arr.length; i++) {
+        runningTotal = callback(arr[i], runningTotal);
+    }
+    return runningTotal;
+}
